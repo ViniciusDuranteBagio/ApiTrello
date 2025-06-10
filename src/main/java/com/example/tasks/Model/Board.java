ca11aeb94f20a53e66c8ac658f3eb744415fe310
felisbinojose <you@example.com>
2025-06-10 20:50:22 -0300

    Feat: adicionado a entidade board

--- src/main/java/com/example/tasks/Model/Board.java
+++ src/main/java/com/example/tasks/Model/Board.java
@@ -1,4 +1,33 @@
 package com.example.tasks.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(min=3, max=100, message = "O nome deve conter entre 3 a 100 caracteres")
+    private String name;
+
+    private String description;
+
+
+
+
 }
--- src/main/java/com/example/tasks/Model/Task.java
+++ src/main/java/com/example/tasks/Model/Task.java
@@ -1,5 +1,6 @@
 package com.example.tasks.Model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,9 +11,11 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@Entity
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String description;
     private String status;
