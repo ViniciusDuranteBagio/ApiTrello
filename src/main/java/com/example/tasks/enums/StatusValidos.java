54f34fabb9e6d379e418f653d8f43493b7f0fd23
felisbinojose <you@example.com>
2025-06-10 21:35:53 -0300

    Feat: enum StatusValidos

--- src/main/java/com/example/tasks/enums/StatusValidos.java
+++ src/main/java/com/example/tasks/enums/StatusValidos.java
@@ -1,4 +1,15 @@
 package com.example.tasks.enums;
 
-public class StatusValidos {
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+public enum StatusValidos {
+    TODO,
+    IN_PROGESS,
+    DONE;
+
+    //Aceita letras maisculas e minusculas
+    @JsonCreator
+    public static StatusValidos fromString(String value) {
+        return StatusValidos.valueOf(value.toUpperCase());
+    }
 }
