8ae923d71b8e7ebdccc630078c4e3e129daf8458
felisbinojose <you@example.com>
2025-06-14 19:11:44 -0300

    feat(service): criado método buscarPorId no BoardService

--- src/main/java/com/example/tasks/Service/BoardService.java
+++ src/main/java/com/example/tasks/Service/BoardService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -26,6 +27,10 @@ public class BoardService {
 
     public List<Board> listarTodos(){
         return boardRepository.findAll();
+    }
+
+    public Optional<Board> buscarPorId(Long id){
+        return  boardRepository.findById(id);
     }
 
 
