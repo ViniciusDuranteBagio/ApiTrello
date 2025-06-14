90d6df5c6f8dfe6ff6d0e84305661f1fb96b6ae9
felisbinojose <you@example.com>
2025-06-14 19:02:17 -0300

    feat(controller): criado endpoint de listarTodos no BoardCOntroller

--- src/main/java/com/example/tasks/Controller/BoardController.java
+++ src/main/java/com/example/tasks/Controller/BoardController.java
@@ -6,10 +6,9 @@ import com.example.tasks.Service.BoardService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/boards")
@@ -22,5 +21,11 @@ public class BoardController {
     public ResponseEntity<Board> criarBoard(@Valid @RequestBody BoardDto boardDto){
         Board boardCriado = boardService.criarBoard(boardDto);
         return ResponseEntity.ok(boardCriado);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Board>> listarTodos(){
+        List<Board> boards = boardService.listarTodos();
+        return ResponseEntity.ok(boards);
     }
 }
