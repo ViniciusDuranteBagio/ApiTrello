a7d1a5e5f4bc88ce1adcb5bca2cc8342f0e50b4e
felisbinojose <you@example.com>
2025-06-14 15:27:24 -0300

    Feat: Adicionado boardService

--- src/main/java/com/example/tasks/Service/BoardService.java
+++ src/main/java/com/example/tasks/Service/BoardService.java
@@ -1,4 +1,23 @@
 package com.example.tasks.Service;
 
+import com.example.tasks.Dtos.BoardDto;
+import com.example.tasks.Model.Board;
+import com.example.tasks.Repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BoardService {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    public Board criarBoard(BoardDto boardDto) {
+        Board board = new Board();
+
+        board.setName(boardDto.getName());
+        board.setDescription(boardDto.getDescription());
+
+        return boardRepository.save(board);
+    }
 }
