59305a57e4bb5141b0a58435448d067f6d6f0baf
felisbinojose <you@example.com>
2025-06-15 14:45:52 -0300

    feat(repository):

--- src/main/java/com/example/tasks/Repository/BoardRepository.java
+++ src/main/java/com/example/tasks/Repository/BoardRepository.java
@@ -2,6 +2,10 @@ package com.example.tasks.Repository;
 
 import com.example.tasks.Model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 }
