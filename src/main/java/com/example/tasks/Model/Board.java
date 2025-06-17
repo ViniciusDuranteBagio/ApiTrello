b23135694ea016d735e21fe325cecb0bbdb68db1
Antônio Marcos Goulart <147720066+Antonio-Marcos-Goulart@users.noreply.github.com>
2025-06-17 20:38:57 -0300

    Criação das classes iniciais do projeto api trello - aula poo

---
+++ src/main/java/com/example/tasks/Model/Board.java
@@ -0,0 +1,37 @@
+package com.example.tasks.Model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor  //*
+@NoArgsConstructor //*
+@Entity
+@Table(name = "board", schema = "api_trello")
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @NotEmpty
+    @Column(name = "board_id")
+    private Long boardId;
+
+    @NotEmpty
+    @Size(min = 3, max = 100, message = "O nome do quadro deve ter entre 3 e 100 caracteres")
+    @Column(name = "board_name")
+    private String boardName;
+
+    @Column(name = "board_description")
+    private String boardDescription;
+
+    @NotEmpty
+    @Column(name = "board_group")
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
+    private String boardGroup;
+
+}
--- src/main/java/com/example/tasks/Model/Task.java
+++ src/main/java/com/example/tasks/Model/Task.java
@@ -1,19 +1,43 @@
 package com.example.tasks.Model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.example.tasks.Controller.TaskGroup;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.NumberFormat;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "task", schema = "api_trello")
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String name;
-    private String description;
-    private String status;
+    @NotEmpty
+    @Column(name = "task_id")
+    private Long taskId;
+
+    @NotEmpty
+    @Size(min = 3, max = 100, message = "O nome do deve ter entre 3 e 100 caracteres")
+    @Column(name = "task_title")
+    private String taskTitle;
+
+    @Column(name = "task_description")
+    private String taskDescription;
+
+    @Enumerated(EnumType.STRING) // *
+    @Column(name = "task_status", nullable = false)
+    private String taskStatus;
+
+    @ManyToOne(optional = false) // *
+    @JoinColumn(name = "task_group_id") // *
+    private TaskGroup taskGroup;
 }
