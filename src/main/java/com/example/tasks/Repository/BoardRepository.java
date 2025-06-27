bd99e7ee4195b26ecdbb645ecfcf3573b195fe1d
yalapereira <yalapereira@gmail.com>
2025-06-27 15:54:38 -0300

    Repository

--- src/main/java/com/example/tasks/Repository/BoardRepository.java
+++ src/main/java/com/example/tasks/Repository/BoardRepository.java
@@ -1,9 +1,9 @@
 package com.example.tasks.Repository;
 
-import com.example.tasks.Model.Task;
+import com.example.tasks.Model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface BoardRepository extends JpaRepository<Board, Long> {
 }
--- src/main/java/com/example/tasks/Repository/TaskGroupRepository.java
+++ src/main/java/com/example/tasks/Repository/TaskGroupRepository.java
@@ -1,9 +1,9 @@
 package com.example.tasks.Repository;
 
-import com.example.tasks.Model.Task;
+import com.example.tasks.Model.TaskGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskGroupRepository extends JpaRepository<TaskGroup, Long> {
 }
